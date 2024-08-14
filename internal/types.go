922318fb235057147bf00bbbd2e90fda037aef34
Karl Cardenas <cardenas88karl@gmail.com>
2024-08-14 14:35:18 -0700

    chore: added csv stuct tags

--- internal/types.go
+++ internal/types.go
@@ -25,146 +25,146 @@ type AuthCredentials struct {
 }
 
 type User struct {
-	UserData           UserData           `json:"user_data,omitempty"`
-	UserMesaurements   UserMesaurements   `json:"user_mesaurements,omitempty"`
-	SleepCollection    SleepCollection    `json:"sleep_collection"`
-	RecoveryCollection RecoveryCollection `json:"recovery_collection"`
-	WorkoutCollection  WorkoutCollection  `json:"workout_collection"`
-	CycleCollection    CycleCollection    `json:"cycle_collection"`
+	UserData           UserData           `json:"user_data,omitempty" csv:"user_data"`
+	UserMesaurements   UserMesaurements   `json:"user_mesaurements,omitempty" csv:"user_mesaurements"`
+	SleepCollection    SleepCollection    `json:"sleep_collection" csv:"sleep_collection"`
+	RecoveryCollection RecoveryCollection `json:"recovery_collection" csv:"recovery_collection"`
+	WorkoutCollection  WorkoutCollection  `json:"workout_collection" csv:"workout_collection"`
+	CycleCollection    CycleCollection    `json:"cycle_collection" csv:"cycle_collection"`
 }
 
 type UserData struct {
-	UserID    int    `json:"user_id"`
-	Email     string `json:"email"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
+	UserID    int    `json:"user_id" csv:"user_id"`
+	Email     string `json:"email" csv:"email"`
+	FirstName string `json:"first_name" csv:"first_name"`
+	LastName  string `json:"last_name" csv:"last_name"`
 }
 
 type UserMesaurements struct {
-	HeightMeter    float64 `json:"height_meter"`
-	WeightKilogram float64 `json:"weight_kilogram"`
-	MaxHeartRate   int     `json:"max_heart_rate"`
+	HeightMeter    float64 `json:"height_meter" csv:"height_meter"`
+	WeightKilogram float64 `json:"weight_kilogram" csv:"weight_kilogram"`
+	MaxHeartRate   int     `json:"max_heart_rate" csv:"max_heart_rate"`
 }
 
 type SleepCollection struct {
-	SleepCollectionRecords []SleepCollectionRecords `json:"records"`
-	NextToken              string                   `json:"next_token,omitempty"`
+	SleepCollectionRecords []SleepCollectionRecords `json:"records" csv:"records"`
+	NextToken              string                   `json:"next_token,omitempty" csv:"next_token,omitempty"`
 }
 type StageSummary struct {
-	TotalInBedTimeMilli         int `json:"total_in_bed_time_milli"`
-	TotalAwakeTimeMilli         int `json:"total_awake_time_milli"`
-	TotalNoDataTimeMilli        int `json:"total_no_data_time_milli"`
-	TotalLightSleepTimeMilli    int `json:"total_light_sleep_time_milli"`
-	TotalSlowWaveSleepTimeMilli int `json:"total_slow_wave_sleep_time_milli"`
-	TotalRemSleepTimeMilli      int `json:"total_rem_sleep_time_milli"`
-	SleepCycleCount             int `json:"sleep_cycle_count"`
-	DisturbanceCount            int `json:"disturbance_count"`
+	TotalInBedTimeMilli         int `json:"total_in_bed_time_milli" csv:"total_in_bed_time_milli"`
+	TotalAwakeTimeMilli         int `json:"total_awake_time_milli" csv:"total_awake_time_milli"`
+	TotalNoDataTimeMilli        int `json:"total_no_data_time_milli" csv:"total_no_data_time_milli"`
+	TotalLightSleepTimeMilli    int `json:"total_light_sleep_time_milli" csv:"total_light_sleep_time_milli"`
+	TotalSlowWaveSleepTimeMilli int `json:"total_slow_wave_sleep_time_milli" csv:"total_slow_wave_sleep_time_milli"`
+	TotalRemSleepTimeMilli      int `json:"total_rem_sleep_time_milli" csv:"total_rem_sleep_time_milli"`
+	SleepCycleCount             int `json:"sleep_cycle_count" csv:"sleep_cycle_count"`
+	DisturbanceCount            int `json:"disturbance_count" csv:"disturbance_count"`
 }
 type SleepNeeded struct {
-	BaselineMilli             int `json:"baseline_milli"`
-	NeedFromSleepDebtMilli    int `json:"need_from_sleep_debt_milli"`
-	NeedFromRecentStrainMilli int `json:"need_from_recent_strain_milli"`
-	NeedFromRecentNapMilli    int `json:"need_from_recent_nap_milli"`
+	BaselineMilli             int `json:"baseline_milli" csv:"baseline_milli"`
+	NeedFromSleepDebtMilli    int `json:"need_from_sleep_debt_milli" csv:"need_from_sleep_debt_milli"`
+	NeedFromRecentStrainMilli int `json:"need_from_recent_strain_milli" csv:"need_from_recent_strain_milli"`
+	NeedFromRecentNapMilli    int `json:"need_from_recent_nap_milli" csv:"need_from_recent_nap_milli"`
 }
 type Score struct {
-	StageSummary               StageSummary `json:"stage_summary"`
-	SleepNeeded                SleepNeeded  `json:"sleep_needed"`
-	RespiratoryRate            float64      `json:"respiratory_rate"`
-	SleepPerformancePercentage float64      `json:"sleep_performance_percentage"`
-	SleepConsistencyPercentage float64      `json:"sleep_consistency_percentage"`
-	SleepEfficiencyPercentage  float64      `json:"sleep_efficiency_percentage"`
+	StageSummary               StageSummary `json:"stage_summary" csv:"stage_summary"`
+	SleepNeeded                SleepNeeded  `json:"sleep_needed" csv:"sleep_needed"`
+	RespiratoryRate            float64      `json:"respiratory_rate" csv:"respiratory_rate"`
+	SleepPerformancePercentage float64      `json:"sleep_performance_percentage" csv:"sleep_performance_percentage"`
+	SleepConsistencyPercentage float64      `json:"sleep_consistency_percentage" csv:"sleep_consistency_percentage"`
+	SleepEfficiencyPercentage  float64      `json:"sleep_efficiency_percentage" csv:"sleep_efficiency_percentage"`
 }
 type SleepCollectionRecords struct {
-	ID             int       `json:"id"`
-	UserID         int       `json:"user_id"`
-	CreatedAt      time.Time `json:"created_at"`
-	UpdatedAt      time.Time `json:"updated_at"`
-	Start          time.Time `json:"start"`
-	End            time.Time `json:"end"`
-	TimezoneOffset string    `json:"timezone_offset"`
-	Nap            bool      `json:"nap"`
-	ScoreState     string    `json:"score_state"`
-	Score          Score     `json:"score"`
+	ID             int       `json:"id" csv:"id"`
+	UserID         int       `json:"user_id" csv:"user_id"`
+	CreatedAt      time.Time `json:"created_at" csv:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at" csv:"updated_at"`
+	Start          time.Time `json:"start" csv:"start"`
+	End            time.Time `json:"end" csv:"end"`
+	TimezoneOffset string    `json:"timezone_offset" csv:"timezone_offset"`
+	Nap            bool      `json:"nap" csv:"nap"`
+	ScoreState     string    `json:"score_state" csv:"score_state"`
+	Score          Score     `json:"score" csv:"score"`
 }
 
 type CycleCollection struct {
-	Records   []CycleRecords `json:"records"`
-	NextToken string         `json:"next_token,omitempty"`
+	Records   []CycleRecords `json:"records" csv:"records"`
+	NextToken string         `json:"next_token,omitempty" csv:"next_token,omitempty"`
 }
 type CycleScore struct {
-	Strain           float64 `json:"strain"`
-	Kilojoule        float64 `json:"kilojoule"`
-	AverageHeartRate int     `json:"average_heart_rate"`
-	MaxHeartRate     int     `json:"max_heart_rate"`
+	Strain           float64 `json:"strain" csv:"strain"`
+	Kilojoule        float64 `json:"kilojoule" csv:"kilojoule"`
+	AverageHeartRate int     `json:"average_heart_rate" csv:"average_heart_rate"`
+	MaxHeartRate     int     `json:"max_heart_rate" csv:"max_heart_rate"`
 }
 type CycleRecords struct {
-	ID             int        `json:"id"`
-	UserID         int        `json:"user_id"`
-	CreatedAt      time.Time  `json:"created_at"`
-	UpdatedAt      time.Time  `json:"updated_at"`
-	Start          time.Time  `json:"start"`
-	End            time.Time  `json:"end"`
-	TimezoneOffset string     `json:"timezone_offset"`
-	ScoreState     string     `json:"score_state"`
-	Score          CycleScore `json:"score"`
+	ID             int        `json:"id" csv:"id"`
+	UserID         int        `json:"user_id" csv:"user_id"`
+	CreatedAt      time.Time  `json:"created_at" csv:"created_at"`
+	UpdatedAt      time.Time  `json:"updated_at" csv:"updated_at"`
+	Start          time.Time  `json:"start" csv:"start"`
+	End            time.Time  `json:"end" csv:"end"`
+	TimezoneOffset string     `json:"timezone_offset" csv:"timezone_offset"`
+	ScoreState     string     `json:"score_state" csv:"score_state"`
+	Score          CycleScore `json:"score" csv:"score"`
 }
 
 type RecoveryCollection struct {
-	RecoveryRecords []RecoveryRecords `json:"records"`
+	RecoveryRecords []RecoveryRecords `json:"records" csv:"records"`
 	NextToken       string            `json:"next_token,omitempty"`
 }
 type RecoveryScore struct {
-	UserCalibrating  bool    `json:"user_calibrating"`
-	RecoveryScore    float64 `json:"recovery_score"`
-	RestingHeartRate float64 `json:"resting_heart_rate"`
-	HrvRmssdMilli    float64 `json:"hrv_rmssd_milli"`
-	Spo2Percentage   float64 `json:"spo2_percentage"`
-	SkinTempCelsius  float64 `json:"skin_temp_celsius"`
+	UserCalibrating  bool    `json:"user_calibrating" csv:"user_calibrating"`
+	RecoveryScore    float64 `json:"recovery_score" csv:"recovery_score"`
+	RestingHeartRate float64 `json:"resting_heart_rate" csv:"resting_heart_rate"`
+	HrvRmssdMilli    float64 `json:"hrv_rmssd_milli" csv:"hrv_rmssd_milli"`
+	Spo2Percentage   float64 `json:"spo2_percentage" csv:"spo2_percentage"`
+	SkinTempCelsius  float64 `json:"skin_temp_celsius" csv:"skin_temp_celsius"`
 }
 type RecoveryRecords struct {
-	CycleID    int           `json:"cycle_id"`
-	SleepID    int           `json:"sleep_id"`
-	UserID     int           `json:"user_id"`
-	CreatedAt  time.Time     `json:"created_at"`
-	UpdatedAt  time.Time     `json:"updated_at"`
-	ScoreState string        `json:"score_state"`
-	Score      RecoveryScore `json:"score"`
+	CycleID    int           `json:"cycle_id" csv:"cycle_id"`
+	SleepID    int           `json:"sleep_id" csv:"sleep_id"`
+	UserID     int           `json:"user_id" csv:"user_id"`
+	CreatedAt  time.Time     `json:"created_at" csv:"created_at"`
+	UpdatedAt  time.Time     `json:"updated_at" csv:"updated_at"`
+	ScoreState string        `json:"score_state" csv:"score_state"`
+	Score      RecoveryScore `json:"score" csv:"score"`
 }
 
 type WorkoutCollection struct {
-	Records   []WorkoutRecords `json:"records"`
+	Records   []WorkoutRecords `json:"records" csv:"records"`
 	NextToken string           `json:"next_token,omitempty"`
 }
 type ZoneDuration struct {
-	ZoneZeroMilli  int `json:"zone_zero_milli"`
-	ZoneOneMilli   int `json:"zone_one_milli"`
-	ZoneTwoMilli   int `json:"zone_two_milli"`
-	ZoneThreeMilli int `json:"zone_three_milli"`
-	ZoneFourMilli  int `json:"zone_four_milli"`
-	ZoneFiveMilli  int `json:"zone_five_milli"`
+	ZoneZeroMilli  int `json:"zone_zero_milli" csv:"zone_zero_milli"`
+	ZoneOneMilli   int `json:"zone_one_milli" csv:"zone_one_milli"`
+	ZoneTwoMilli   int `json:"zone_two_milli" csv:"zone_two_milli"`
+	ZoneThreeMilli int `json:"zone_three_milli" csv:"zone_three_milli"`
+	ZoneFourMilli  int `json:"zone_four_milli" csv:"zone_four_milli"`
+	ZoneFiveMilli  int `json:"zone_five_milli" csv:"zone_five_milli"`
 }
 type WorkoutScore struct {
-	Strain              float64      `json:"strain"`
-	AverageHeartRate    int          `json:"average_heart_rate"`
-	MaxHeartRate        int          `json:"max_heart_rate"`
-	Kilojoule           float64      `json:"kilojoule"`
-	PercentRecorded     float64      `json:"percent_recorded"`
-	DistanceMeter       float64      `json:"distance_meter"`
-	AltitudeGainMeter   float64      `json:"altitude_gain_meter"`
-	AltitudeChangeMeter float64      `json:"altitude_change_meter"`
-	ZoneDuration        ZoneDuration `json:"zone_duration"`
+	Strain              float64      `json:"strain" csv:"strain"`
+	AverageHeartRate    int          `json:"average_heart_rate" csv:"average_heart_rate"`
+	MaxHeartRate        int          `json:"max_heart_rate" csv:"max_heart_rate"`
+	Kilojoule           float64      `json:"kilojoule" csv:"kilojoule"`
+	PercentRecorded     float64      `json:"percent_recorded" csv:"percent_recorded"`
+	DistanceMeter       float64      `json:"distance_meter" csv:"distance_meter"`
+	AltitudeGainMeter   float64      `json:"altitude_gain_meter" csv:"altitude_gain_meter"`
+	AltitudeChangeMeter float64      `json:"altitude_change_meter" csv:"altitude_change_meter"`
+	ZoneDuration        ZoneDuration `json:"zone_duration" csv:"zone_duration"`
 }
 type WorkoutRecords struct {
-	ID             int          `json:"id"`
-	UserID         int          `json:"user_id"`
-	CreatedAt      time.Time    `json:"created_at"`
-	UpdatedAt      time.Time    `json:"updated_at"`
-	Start          time.Time    `json:"start"`
-	End            time.Time    `json:"end"`
-	TimezoneOffset string       `json:"timezone_offset"`
-	SportID        int          `json:"sport_id"`
-	ScoreState     string       `json:"score_state"`
-	Score          WorkoutScore `json:"score"`
+	ID             int          `json:"id" csv:"id"`
+	UserID         int          `json:"user_id" csv:"user_id"`
+	CreatedAt      time.Time    `json:"created_at" csv:"created_at"`
+	UpdatedAt      time.Time    `json:"updated_at" csv:"updated_at"`
+	Start          time.Time    `json:"start" csv:"start"`
+	End            time.Time    `json:"end" csv:"end"`
+	TimezoneOffset string       `json:"timezone_offset" csv:"timezone_offset"`
+	SportID        int          `json:"sport_id" csv:"sport_id"`
+	ScoreState     string       `json:"score_state" csv:"score_state"`
+	Score          WorkoutScore `json:"score" csv:"score"`
 }
 
 /*
